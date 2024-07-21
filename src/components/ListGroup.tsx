45f1b43f7161ada807688df75164b814af45ecea
II-777 <45958058+II-777@users.noreply.github.com>
2024-07-20 23:53:31 -0400

    conditional-rendering-3

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,18 +2,13 @@ function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
 
-  // const message = items.length === 0 ? <p>No items found</p> : null
-  // const message = items.length === 0 && <p>No items found</p>
-  
-  const getMessage = () => {
-    return items.length === 0 ? <p>No items found</p> : null;
-  };
+  const message = items.length === 0 && <p>No items found</p>;
 
   if (items.length === 0)
     return (
       <>
         <h1>List</h1>
-        {getMessage()}
+        {message}
       </>
     );
 
